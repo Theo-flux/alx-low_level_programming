28c73428fc510a992cb5400a039a614d69bb3d92
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-07 08:15:24 -0700

    fix on 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  * Return: 0
  */
 
-int main(void){
-puts( "\"Programming is like building a multilingual puzzle\n" );
+int main(void)
+{
+puts( "\"Programming is like building a multilingual puzzle" );
 return(0);
 }
 
