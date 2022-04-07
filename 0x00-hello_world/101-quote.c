cfc7def5d2d01493b6cd2021d428f43a3116a046
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-07 08:53:59 -0700

    101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * Return: Always 1 (error)
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
-
