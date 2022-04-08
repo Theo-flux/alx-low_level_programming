045471d817dd90afd7af076f5aa4104cad27fe91
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-08 04:42:34 -0700

    betty style implementation on task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes here */
-if(n==0)
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
-else if(n>0)
+else if (n > 0)
 {
 printf("%i is positive\n", n);
 }
