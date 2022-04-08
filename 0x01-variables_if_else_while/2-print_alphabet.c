3e3e8a57d5ca5a8e6fee98364c62eaeb6ab57efd
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-08 13:14:52 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+#include <string.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
-
+/*
+ * main function
+ * return 0 always (Success)
+ */
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-
-	return (0);	
+int n;
+char str;
+str = "abcdefghijklmnopqrstuvwxyz";
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+for (int i = 0; i < strlen(str); i++)
+{
+putchar("%c", tolower((unsigned char) str[i]));
+}
+putchar("\n");
+return (0);	
 }
