6823a7cb9775ff69aaf6f8e489e06dbbb4628ace
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-09 09:33:11 -0700

    6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("Size of  char: %d bytes(s)\n", sizeof(char));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
