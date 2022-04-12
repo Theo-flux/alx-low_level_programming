78194c3e6704d9f5ec531c2bb48f314724370803
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-12 10:24:35 -0700

    0-putchar.c task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry for main function
+ * Description: Program to print _putchar, followed by a line
+ * return: 0 Always (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
+
