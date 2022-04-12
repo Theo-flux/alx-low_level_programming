53f9cbdc4929aa1ce83dfc2b4f67af75b433b4e9
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-12 16:43:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 /**
  * jack_bauer - Entry function
- * 
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-int i,m;
+int i, m;
 for (i = 0; i < 24; ++i)
 {
 for (m = 0; m < 60; ++m)
 {
-if(i < 10)
+if (i < 10)
 {
 printf("0");
 }
 printf("%d", i);
 printf(":");
-if(m < 10)
+if (m < 10)
 {
 printf("0");
 }
