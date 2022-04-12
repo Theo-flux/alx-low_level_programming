ea9437e244ba30fff2008a060eb7afe7ffaf62ba
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-12 12:18:50 -0700

    added 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  print alphabet 10x
+ *  Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+char c;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
+}
