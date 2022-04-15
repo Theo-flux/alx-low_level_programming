ba85622bc48af9127ef220ba4fb17b7aecbd80a0
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-15 12:11:11 -0700

    task 2 fix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - Enty function
+ * @n: int number type
+ * Return - void
+ */
+void print_line(int n)
+{
+int i;
+for(i = 0; i < n; ++i)
+{
+
+}
+_putchar('\n');
+}
