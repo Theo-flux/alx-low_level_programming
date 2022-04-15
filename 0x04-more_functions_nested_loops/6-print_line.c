9043d2008b27a6e038506002835d793a79c5610e
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-15 13:26:50 -0700

    hot fiz task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_line - Enty function
- * @n: int number type
+ * print_line - check for a digit
+ * @n : number of _ to be printed
  * Return:void
  */
 void print_line(int n)
 {
-int i;
-for(i = 0; i < n; ++i)
+int i = 0;
+while (i < n && n > 0)
 {
-_putchar('-');
+_putchar('_');
+i++;
 }
 _putchar('\n');
 }
