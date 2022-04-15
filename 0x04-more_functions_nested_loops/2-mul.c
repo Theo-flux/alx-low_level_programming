82cefba503052a5d4b4ddde12e0015c686a53249
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-15 04:21:49 -0700

    2-mul c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Entry function
+ * @a: first operand
+ * @b: second operand
+ * Return: Always 0 or 1.
+ */
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return product;
+}
