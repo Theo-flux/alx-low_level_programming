6b2efa778919138c25e88a144c034be6cdd36e49
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-20 07:49:38 -0700

    mandatory task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry function
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+void reset_to_98(int *n);
+#endif
