d55a92f06a8f2faf442e85b2da72d26364f771b1
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-20 09:45:40 -0700

    mandatory task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,13 +2,13 @@
 #include <string.h>
 /**
  * print_rev - Entry function
- * @s: pointer to char 
+ * @s: pointer to char
  * Return: 0
  */
 void print_rev(char *s)
 {
 char rev[1000];
-int i,j;
+int i, j;
 int length = strlen(s);
 j = length - 1;
 for (i = 0; i < length; ++i)
