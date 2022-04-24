8610271374e05aac09283f73b2e00a2ca8054692
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-24 09:29:24 -0700

    task 5 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper(char *s) -  Entry function
+ * @str: pointer to char
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+str[i] -= 32;
+}
+return (str);
+}
