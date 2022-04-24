82d5bb901328a3233b9832b3995e7d1a88c320cc
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-24 05:17:24 -0700

    1-strncat.c task refactoring

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i, dest_len;
 dest_len = strlen(dest);
-for (i = 0; i < n; ++i)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[dest_len + i] = src[i];
 }
+dest[dest_len + i] = '\0';
 return (dest);
 }
