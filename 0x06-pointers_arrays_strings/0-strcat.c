79a7ae5f0641f914e00b79e737e3c73594cead65
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-24 04:11:55 -0700

    mandatory task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - Entry Function
+ * @dest: pointer to char type
+ * @src: pointer to char type
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
