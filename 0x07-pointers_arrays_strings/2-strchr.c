411c8dcb7b71e59fb63082b6bfb10d30e97ecefd
Theo Phil <ekunnusitheophilus@gmail.com>
2022-04-26 10:49:46 -0700

    hot fix on task-2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+while (*s != '\0')
+{
+	if (*s == c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+		return (s);
 	}
+	s++;
+}
 return (0);
 }
