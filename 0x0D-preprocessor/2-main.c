08523c67603faf381466e4aa2b804de5831f98e3
Theo Phil <ekunnusitheophilus@gmail.com>
2022-05-12 05:24:17 -0700

    task-2 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry function
- * 
- * Return: Always 0
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
