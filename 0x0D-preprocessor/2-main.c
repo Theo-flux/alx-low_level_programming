7b20bc3c536f0db2bfc6a485e0c1d4293a564882
Theo Phil <ekunnusitheophilus@gmail.com>
2022-05-12 05:23:19 -0700

    task-2 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * main - Entry function
  * 
- * Return: 0
+ * Return: Always 0
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
