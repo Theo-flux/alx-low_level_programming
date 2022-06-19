c70cd907a6cca60e2ad906fca2e1e3349e72e024
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2022-06-19 12:50:28 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,13 @@
  */
 void puts_half(char *str)
 {
-int i, start, count;
-count = strlen(str);
-if (count % 2)
-{
-start = count / 2;
-}
-else
-{
-start = (count - 1) / 2;
-}
-for (i = start + 1; i < count; ++i)
-{
-printf("%c", str[i]);
-}
-printf("\n");
+    int half;
+    int size = strlen(str);
+    half = size/2;
+
+    while (str[half] != '\0'){
+        _putchar(str[half]);
+        half++;
+    }
+    _putchar('\n');
 }
