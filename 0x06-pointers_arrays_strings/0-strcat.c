af0b3ecbd72fa0fe320d3e7f51d996fa5e2f560c
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2022-06-19 17:29:17 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i, dest_len, src_len;
-dest_len = strlen(dest);
-src_len = strlen(src);
-for (i = 0; i < src_len + 1; ++i)
-{
-dest[dest_len + i] = src[i];
-}
-return (dest);
+    char *srcPtr = src;
+    int i, srcLen, destLen;
+
+    srcLen = strlen(src);
+    destLen = strlen(dest);
+
+    for (i = 0; i < srcLen; i++)
+    {
+        dest[destLen + i] = *srcPtr++;
+    }
+
+    return (dest);
 }
