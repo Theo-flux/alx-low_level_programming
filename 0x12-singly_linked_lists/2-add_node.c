b649865cd10471b98b0df4ea09fdf5dbeb6eb285
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2022-07-01 14:41:18 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,28 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *newNode;
 
-	while (str[len])
-		len++;
+        newNode = malloc(sizeof(list_t));
 
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
+        if(newNode == NULL)
+        {
+                free(newNode);
+                return NULL;
+        }
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+        newNode->str = strdup(str);
+        newNode->len = strlen(str);
 
-	return (*head);
+        if(*head == NULL)
+        {
+                (*head) = newNode;
+        }
+        else
+        {
+                newNode->next = *head;
+                (*head) = newNode;
+        }
+        return (*head);
+
 }
