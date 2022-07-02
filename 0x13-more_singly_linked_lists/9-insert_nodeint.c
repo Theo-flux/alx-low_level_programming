80199bc9ec838868d5eef04495c8715250da6521
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2022-07-02 22:59:25 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,33 +9,39 @@
  * Return: Address of the new node.
  **/
 
+
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
-	unsigned int i = 0;
+        listint_t *node, *tmp;
+        unsigned int i = 0;
+        unsigned int size = listint_len((*head));
 
-	if (*head == NULL && idx != 0)
-		return (NULL);
-	if (idx != 0)
-	{
-	temp = *head;
-	for (; i < idx - 1 && temp != NULL; i++)
-		temp = temp->next;
-	if (temp == NULL)
-		return (NULL);
-	}
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+        node = (listint_t *) malloc(sizeof(listint_t));
+        tmp = *head;
+
+        if (node == NULL)
+        {
+                free(NULL);
+        }
+
+        if (idx > size)
+        {
+                return NULL;
+        }
+        else
+        {
+                while ( i < idx)
+                {
+                        tmp = tmp->next;
+                        i++;
+                }
+
+                node->n = n;
+                node->next = tmp->next;
+                tmp->next = node;
+
+                return node;
+        }
+
 }
-
