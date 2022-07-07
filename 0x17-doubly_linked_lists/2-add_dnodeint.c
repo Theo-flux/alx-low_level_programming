2b762de6ad577856355e4581b4d43d45d51394c2
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-07 08:43:46 -0700

    tiredgit add .git add .!

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,8 +11,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
-	newNode = (dlistint_t *) malloc(sizeof(dlistint_t));
-	
+
+	newNode = (dlistint_t *) malloc(sizeof(dlistint_t));	
 	if (newNode == NULL)
 	{
 		return (NULL);
