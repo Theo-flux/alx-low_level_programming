030e58d2cab3a2cd038ef9b0afaa8ee9f0e7645a
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-07 07:23:25 -0700

    number of elements in list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stddef.h>
+
+/*
+ * dlistint_len - Function taht returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to head
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	
+	return i;
+}
