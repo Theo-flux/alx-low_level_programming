a67ea6ce76871d8e74af5a042b73cc4f55c6dbb9
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-07 07:32:54 -0700

    fix again

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
+
 /*
- * dlistint_len - Function taht returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - number of elements in a linked dlistint_t list.
  * @h: pointer to head
- *
  * Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
