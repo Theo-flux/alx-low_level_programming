98fe1386d1535ddcc57a576fa3d139448e77b748
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-12 09:03:09 -0700

    100-operations.so dynamic lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
 #ifndef DYNAMIC_LIB_H
 #define DYNAMIC_LIB_H
 
+#include <stdlib.h>
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +24,13 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+
+/*op prototype*/
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif
